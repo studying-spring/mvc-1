12dd2d5486916e5f9bd42ba6ed1cb4978c6939c8
leehjhjhj <tsukiakarii@naver.com>
2023-03-23 14:27:50 +0900

    1, 2주차

---
+++ 현제/1,2주차/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,45 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name= "requestHeaerServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        printStartLine(request);
+        printHeaders(request);
+    }
+
+    private static void printStartLine(HttpServletRequest request) {
+        System.out.println("--- REQUEST-LINE - start ---");
+        System.out.println("request.getMethod() = " + request.getMethod()); //GET
+        System.out.println("request.getProtocol() = " + request.getProtocol()); //
+        System.out.println("request.getScheme() = " + request.getScheme()); //http
+        // http://localhost:8080/request-header
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+        // /request-header
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+        //username=hi
+        System.out.println("request.getQueryString() = " +
+                request.getQueryString());
+        System.out.println("request.isSecure() = " + request.isSecure());
+        System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+
+    }
+    private static void printHeaders(HttpServletRequest request) {
+        System.out.println("--- Headers - start ---");
+        request.getHeaderNames().asIterator()
+                        .forEachRemaining(headerName -> System.out.println("headerName = " + headerName));
+        System.out.println("--- Headers - end ---");
+        System.out.println();
+
+    }
+
+}
